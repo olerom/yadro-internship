89133654f83613ceb2408f152791e0fcd7650485
olerom <mefiader@gmail.com>
2017-12-01 07:09:04 +0300

    Refactor inlined CliApp code

--- cli/src/main/java/com/yadro/CliApp.java
+++ cli/src/main/java/com/yadro/CliApp.java
@@ -1,9 +1,9 @@
 package com.yadro;
 
 
-import jdk.nashorn.internal.ir.CatchNode;
 import one.nio.http.HttpException;
 import one.nio.pool.PoolException;
+import org.jetbrains.annotations.NotNull;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -15,8 +15,19 @@ import java.io.InputStreamReader;
  * @author olerom
  */
 public class CliApp {
+
+    @NotNull
+    private Client client;
+
     public static void main(String[] args) {
-        final Client client = new Client();
+        new CliApp(new Client()).run();
+    }
+
+    public CliApp(@NotNull Client client) {
+        this.client = client;
+    }
+
+    public void run() {
         final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
         while (true) {
@@ -32,58 +43,21 @@ public class CliApp {
                 switch (input[1]) {
                     case "h":
                     case "help":
-                        System.out.println(client.getHelp());
+                        handleHelp();
                         break;
 
                     case "list":
-                        if (input.length != 6) {
-                            System.out.println("Not enough arguments: 7 expected");
-                            break;
-                        }
-
-                        if (!input[2].equals("--server") || !input[4].equals("--port")) {
-                            System.out.println("Don't forget about flags");
-                            break;
-                        }
-
-                        final String server0 = input[3];
-                        try {
-                            final int port0 = Integer.parseInt(input[5]);
-                            System.out.println(client.getInterfaceNames(server0, port0));
-                        } catch (NumberFormatException e) {
-                            System.out.println("Port is not a digit value");
-                        } catch (InterruptedException | IOException | HttpException | PoolException e) {
-                            System.out.println("Make sure that server or port arguments were correct");
-                        }
-
+                        handleList(input);
                         break;
+
                     case "show":
-                        if (input.length != 7) {
-                            System.out.println("Not enough arguments: 7 expected");
-                            break;
-                        }
-
-                        if (!input[3].equals("--server") || !input[5].equals("--port")) {
-                            System.out.println("Don't forget about flags");
-                            break;
-                        }
-
-                        final String interfaceName = input[2];
-                        final String server1 = input[4];
-
-                        try {
-                            final int port1 = Integer.parseInt(input[6]);
-                            System.out.println(client.getInterfaceInfo(interfaceName, server1, port1));
-                        } catch (NumberFormatException e) {
-                            System.out.println("Port is not a digit value");
-                        } catch (InterruptedException | IOException | HttpException | PoolException e) {
-                            System.out.println("Make sure that server or port arguments were correct");
-                        }
+                        handleShow(input);
                         break;
+
                     case "q":
                     case "quit":
-                        System.out.println("Quitting");
-                        return;
+                        handleQuit();
+                        break;
 
                     default:
                         System.out.println("Make sure that ur commands are correct. Look for help: cli_net h");
@@ -92,5 +66,60 @@ public class CliApp {
                 System.out.println("Unsupported util");
             }
         }
+    }
+
+    private void handleQuit() {
+        System.out.println("Quitting");
+        System.exit(0);
+    }
+
+    private void handleShow(@NotNull final String[] input) {
+        if (input.length != 7) {
+            System.out.println("Not enough arguments: 7 expected");
+            return;
+        }
+
+        if (!input[3].equals("--server") || !input[5].equals("--port")) {
+            System.out.println("Don't forget about flags");
+            return;
+        }
+
+        final String interfaceName = input[2];
+        final String server = input[4];
+
+        try {
+            final int port = Integer.parseInt(input[6]);
+            System.out.println(client.getInterfaceInfo(interfaceName, server, port));
+        } catch (NumberFormatException e) {
+            System.out.println("Port is not a digit value");
+        } catch (InterruptedException | IOException | HttpException | PoolException e) {
+            System.out.println("Make sure that server or port arguments were correct");
+        }
+    }
+
+    private void handleList(@NotNull final String[] input) {
+        if (input.length != 6) {
+            System.out.println("Not enough arguments: 7 expected");
+            return;
+        }
+
+        if (!input[2].equals("--server") || !input[4].equals("--port")) {
+            System.out.println("Don't forget about flags");
+            return;
+        }
+
+        final String server = input[3];
+        try {
+            final int port = Integer.parseInt(input[5]);
+            System.out.println(client.getInterfaceNames(server, port));
+        } catch (NumberFormatException e) {
+            System.out.println("Port is not a digit value");
+        } catch (InterruptedException | IOException | HttpException | PoolException e) {
+            System.out.println("Make sure that server or port arguments were correct");
+        }
+    }
+
+    private void handleHelp() {
+        System.out.println(client.getHelp());
     }
 }
